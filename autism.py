7f313d54e50cda842eade1cbd9fff565f6bdcb02
WarrenKTan <w125t659@ku.edu>
2025-01-11 03:49:57 -0600

    Dmax/Gmax Comparison

--- autism.py
+++ autism.py
@@ -3,41 +3,91 @@
 # Date Modified: 14/12/2024
 
 import csv
-import math
 from pokemon import Pokemon
 
 table = []
 
 def main():
     statsPath = "Pokemon.csv"
-    
+
     createTable(statsPath)
 
-    pokemon1 = findByName("Blastoise")
-    pokemon2 = findByName("Kingler")
+    # pretty sure you have to add every dynamax pokemon manually :/
+    Available_Max_Pokemon = [MaxPokemon("Lapras", "Ice", True), 
+                             MaxPokemon("Charizard", "Fire", True), 
+                             MaxPokemon("Charizard", "Fire, Flying, Dragon*"), 
+                             MaxPokemon("Blastoise", "Water", True), 
+                             MaxPokemon("Blastoise", "Dark, Water"), 
+                             MaxPokemon("Venusaur", "Grass", True),
+                             MaxPokemon("Venusaur", "Grass"), 
+                             MaxPokemon("Gengar", "Ghost", True), 
+                             MaxPokemon("Gengar", "Ghost, Dark"),  
+                             MaxPokemon("Toxtricity", "Electric", True), 
+                             MaxPokemon("Toxtricity", "Electric, Poison"), 
+                             MaxPokemon("Cinderace", "Normal, Fire"), 
+                             MaxPokemon("Inteleon", "Normal, Water"), 
+                             MaxPokemon("Rillaboom", "Normal, Grass"), 
+                             MaxPokemon("Excadrill", "Steel, Ground"), 
+                             MaxPokemon("Articuno", "Ice"), 
+                             MaxPokemon("Zapdos", "Electric"), 
+                             MaxPokemon("Moltres", "Fire, Flying"), 
+                             MaxPokemon("Cryogonal", "Ice"), 
+                             MaxPokemon("Metagross", "Psychic, Steel"), 
+                             MaxPokemon("Greedent", "Normal, Grass, Dark, Ground"), 
+                             MaxPokemon("Dubwool", "Normal, Fighting"), 
+                             MaxPokemon("Kingler", "Water, Steel"), 
+                             MaxPokemon("Machamp", "Steel, Fighting"), 
+                             MaxPokemon("Falinks", "Fighting"), 
+                             ]
+
+    # while i < len(Available_Max_Pokemon)
+
+    Available_Max_Pokemon.sort(reverse=True)
+
+    print("Available Max Pokemon:")
+    print("\033[1mMax    |   Pokemon      |  Damage       |  Type")
+    print(f"{7 * '-'}+{16 * '-'}+{15 * '-'}+{20 * '-'}", end="\033[0m\n")
+    for pokemon in Available_Max_Pokemon:
+        print(pokemon)
+
+
+
+class MaxPokemon:
+
+    def __init__(self, identifier, types, isGMax : bool = False):
+        self.isGMax = isGMax
+        self.types = types
+
+        self.pokemon = findByName(identifier)
+        self.damage = calculateDamage(self.pokemon, 450 if isGMax else 350)
+
+    def __str__(self):
+        form = f"\033[1m{'G' if self.isGMax else 'D'}\033[0mMax"
+        output = f"{form}   |   {self.pokemon.name}\t|  {self.damage} dmg\t|  {self.types}"
+        # print(output)
+        return output
+
+    def __gt__(self, other):
+        return self.damage > other.damage
+    
+    def __lt__(self, other):
+        return self.damage < other.damage
+    
+    def __eq__(self, other):
+        return self.damage == other.damage
+
 
     
-    
-    calculateDamage(pokemon1, 350, 15)
-    calculateDamage(pokemon2, 250, 10)
-
-
-
 # creates table of all pokemon and their stats
 def createTable(filePath : str):
-
     with open(filePath, "r") as file:
         csvReader = csv.reader(file)
         for data in csvReader:
-
             # exclude format line
             if data[0] != "ID":
                 # create new entry
                 newEntry = Pokemon(data[0], data[1], data[2], int(data[6]), int(data[7]), int(data[8]), int(data[9]), int(data[10]), int(data[11]))
                 table.append(newEntry)
-
-            if data[0] == 151:
-                break
 
 def findByName(name : str):
     split = name.find(" ")
@@ -52,13 +102,13 @@ def findByName(name : str):
 
     nameFound = False
     for pokemon in table:
-        if pokemon.name == name and pokemon.form == form:
+        if pokemon.name == name and (form == " " or pokemon.form == form):
             nameFound = True
             returnPokemon = pokemon
             break
 
     if not nameFound:
-        raise ValueError("Name not found")
+        raise ValueError(f"Name not found: \"{name}\"")
 
     return returnPokemon
 
@@ -71,15 +121,14 @@ def findByNum(num : str):
             break
 
     if not nameFound:
-        raise ValueError("Name not found")
+        raise ValueError(f"Num not found: \"{num}\"")
     
     return returnPokemon
 
 def calculateDamage(pokemon : Pokemon, basePower : int, atkIV : int = 10):
     actualAttack = pokemon.attack + atkIV
     actualDamage =  int(((0.5 * basePower * actualAttack) // 1) + 1)
-    
-    print(f"%-10s does {actualDamage} damage with a {basePower} power move." % pokemon.name)
+
     return actualDamage
  
 if __name__ == "__main__":
