3b2309ba8217ec180582e586a452c045ec729464
WarrenKTan <w125t659@ku.edu>
2025-05-30 19:07:15 -0500

    Version 1.0

--- autism.py
+++ autism.py
@@ -1,70 +1,139 @@
 # Author: Warren Tan
 # Date Created: 14/12/2024
-# Date Modified: 14/12/2024
+# Date Modified: 22/05/2025
 
-import csv
-from pokemon import Pokemon
+import re
+import json
+from dataclasses import dataclass, field, asdict
+from typing import List
 
-table = []
+# path to data
+dataPath = "latest.json"
+
+global pokedex
+global moves
+global cpMultipliers
+global typeChart
 
 def main():
-    statsPath = "Pokemon.csv"
+    defender = getPokemon("Rillaboom")
 
-    createTable(statsPath)
+    
 
-    # pretty sure you have to add every dynamax pokemon manually :/
-    Available_Max_Pokemon = [MaxPokemon("Lapras", "Ice", True), 
-                             MaxPokemon("Charizard", "Fire", True), 
-                             MaxPokemon("Charizard", "Fire, Flying, Dragon*"), 
-                             MaxPokemon("Blastoise", "Water", True), 
-                             MaxPokemon("Blastoise", "Dark, Water"), 
-                             MaxPokemon("Venusaur", "Grass", True),
-                             MaxPokemon("Venusaur", "Grass"), 
-                             MaxPokemon("Gengar", "Ghost", True), 
-                             MaxPokemon("Gengar", "Ghost, Dark"),  
-                             MaxPokemon("Toxtricity", "Electric", True), 
-                             MaxPokemon("Toxtricity", "Electric, Poison"), 
-                             MaxPokemon("Cinderace", "Normal, Fire"), 
-                             MaxPokemon("Inteleon", "Normal, Water"), 
-                             MaxPokemon("Rillaboom", "Normal, Grass"), 
-                             MaxPokemon("Excadrill", "Steel, Ground"), 
-                             MaxPokemon("Articuno", "Ice"), 
-                             MaxPokemon("Zapdos", "Electric"), 
-                             MaxPokemon("Moltres", "Fire, Flying"), 
-                             MaxPokemon("Cryogonal", "Ice"), 
-                             MaxPokemon("Metagross", "Psychic, Steel"), 
-                             MaxPokemon("Greedent", "Normal, Grass, Dark, Ground"), 
-                             MaxPokemon("Dubwool", "Normal, Fighting"), 
-                             MaxPokemon("Kingler", "Water, Steel"), 
-                             MaxPokemon("Machamp", "Steel, Fighting"), 
-                             MaxPokemon("Falinks", "Fighting"), 
-                             ]
+    # get max pokemon from txt file
+    with open("availableMax.txt", 'r', encoding="utf-16") as f:
+        pokemonStr = [line.strip("\n") for line in f]
+        maxAttackers = [getPokemon(string) for string in pokemonStr]
 
-    # while i < len(Available_Max_Pokemon)
+    # get Gmax pokemon from txt file
+    with open("availableGMax.txt", 'r', encoding="utf-16") as f:
+        pokemonStr = []
+        for line in f:
+            lineArr = line.strip("\n").split(",")
+            GmaxPokemonInfo = lineArr[0], lineArr[1]
+            pokemonStr.append(GmaxPokemonInfo)
 
-    Available_Max_Pokemon.sort(reverse=True)
+        GmaxAttackers = [getPokemon(GmaxPokemonInfo[0]) for GmaxPokemonInfo in pokemonStr]
 
-    print("Available Max Pokemon:")
-    print("\033[1mMax    |   Pokemon      |  Damage       |  Type")
-    print(f"{7 * '-'}+{16 * '-'}+{15 * '-'}+{20 * '-'}", end="\033[0m\n")
-    for pokemon in Available_Max_Pokemon:
-        print(pokemon)
+        for index, attacker in enumerate(GmaxAttackers):
+            attacker.gmaxType = pokemonStr[index][1]
+
+    # calculate damage and store
+    finalDamagesDict = {}
+
+    # Dmax Attackers
+    for attacker in maxAttackers:
+        for moveStr in attacker.fastMoves:
+            attackerMove = Move(name=moveStr, type=getMove(moveStr).type, power=350)
+            pokemonName, pokemonDamage, moveType = attacker.calculateDamage(attackerMove, defender, printOut = False)
+            finalDamagesDict["Dmax", pokemonName, moveType] = pokemonDamage
+
+        for moveStr in attacker.eliteFastMove:
+            attackerMove = Move(name=moveStr, type=getMove(moveStr).type, power=350)
+            pokemonName, pokemonDamage, moveType = attacker.calculateDamage(attackerMove, defender, printOut = False)
+            if ("Dmax",pokemonName, moveType) not in finalDamagesDict:
+                finalDamagesDict["Dmax", pokemonName, moveType + "*"] = pokemonDamage
+
+    # Gmax Attackers
+    for attacker in GmaxAttackers:
+        if attacker.gmaxType != "typeless":
+            attackerMove = Move(name="GmaxMove", type=(f"POKEMON_TYPE_{attacker.gmaxType}".upper()), power=450)
+            pokemonName, pokemonDamage, moveType = attacker.calculateDamage(attackerMove, defender, printOut = False)
+            finalDamagesDict["Gmax", pokemonName, moveType] = pokemonDamage
+    
+    # convert dictionary to sorted array and print
+    finalDamagesSorted = sorted(finalDamagesDict.items(), key=lambda x: x[1], reverse=True)
+    print(f"Max,Name,Damage,Type")
+    for item in finalDamagesSorted:
+        key, pokemonDamage = item
+        maxType, pokemonName, moveType = key
+        print(f"{maxType},{pokemonName.capitalize()},{pokemonDamage},{moveType.replace('POKEMON_TYPE_', '').capitalize()}")
 
 
+@dataclass
+class Pokemon:
 
-class MaxPokemon:
+    id: int = 0
+    name: str = "missingNo"
+    type: str = "typeless"
+    type2: str = "None"
+    stamina: int = 0
+    attack: int = 0
+    defense: int = 0
+    gmaxType: str = "typeless"
 
-    def __init__(self, identifier, types, isGMax : bool = False):
-        self.isGMax = isGMax
-        self.types = types
+    fastMoves: List[str] = field(default_factory=list)
+    eliteFastMove: List[str] = field(default_factory=list)
+    chargedMoves: List[str] = field(default_factory=list)
+    eliteChargedMove: List[str] = field(default_factory=list)
 
-        self.pokemon = findByName(identifier)
-        self.damage = calculateDamage(self.pokemon, 450 if isGMax else 350)
+    damage: int = None
+
+    def calculateDamage(self, move, other = None, level : int = 20, iv : int = 10, otherLevel : int = 52, printOut = True):
+
+        # default to 0 defense
+        if other is None:
+            other = Pokemon()
+
+        actualAttack = (self.attack * cpMultipliers[level - 1]) + iv
+
+        # Tier 6 max raids have a CP multiplier of ~8.5x = ~level 52
+        # raid boss attack/defense IVs are always maxed
+        actualDefense = (other.defense * cpMultipliers[otherLevel - 1]) + 15
+
+        # STAB
+        sameTypeAttackBonus = (1.2 if ((move.type == self.type) | (move.type == self.type2)) else 1)
+        
+        # supereffective
+        supereffectiveType1 = (1 if other.type == "typeless" else getTypeMatchup(move.type, other.type))
+        supereffectiveType2 = (1 if other.type2 == "" else getTypeMatchup(move.type, other.type2))
+        supereffective = supereffectiveType1 * supereffectiveType2
+
+        modifier = sameTypeAttackBonus * supereffective
+
+        self.damage = int(((0.5 * move.power * (actualAttack/actualDefense) * modifier) // 1) + 1)
+
+        if printOut:
+            print(f"{self.name},{self.damage},{move.type.replace('POKEMON_TYPE_', '').capitalize()}")
+
+        return self.name, self.damage, move.type
 
     def __str__(self):
-        form = f"\033[1m{'G' if self.isGMax else 'D'}\033[0mMax"
-        output = f"{form}   |   {self.pokemon.name}\t|  {self.damage} dmg\t|  {self.types}"
-        # print(output)
+        output = ""
+        for key, value in asdict(self).items():
+            if key not in {"fastMoves", "eliteFastMove", "chargedMoves", "eliteChargedMove"}:
+                output += f"{value},"
+            else:
+                for move in value:
+                    output += f"{move} "
+                output += f","
+
+        return output
+    
+    def keys(self):
+        output = ""
+        for key, _ in asdict(self).items():
+            output += f"{key},"
         return output
 
     def __gt__(self, other):
@@ -76,60 +145,219 @@ class MaxPokemon:
     def __eq__(self, other):
         return self.damage == other.damage
 
+@dataclass
+class Move:
 
+    name: str = ""
+    type: str = "None"
+    power: int = -1
+    energy: int = -1
+
+    def __str__(self):
+        output = ""
+        for key, value in asdict(self).items():
+            output += f"{value},"
+
+        return output
     
-# creates table of all pokemon and their stats
-def createTable(filePath : str):
-    with open(filePath, "r") as file:
-        csvReader = csv.reader(file)
-        for data in csvReader:
-            # exclude format line
-            if data[0] != "ID":
-                # create new entry
-                newEntry = Pokemon(data[0], data[1], data[2], int(data[6]), int(data[7]), int(data[8]), int(data[9]), int(data[10]), int(data[11]))
+    def keys(self):
+        output = ""
+        for key, _ in asdict(self).items():
+            output += f"{key},"
+        return output
+
+    def __gt__(self, other):
+        return self.name > other.name
+    
+    def __lt__(self, other):
+        return self.name < other.name
+    
+    def __eq__(self, other):
+        return self.name == other.name
+
+# creates table
+def createPokemonTable(filePath : str):
+    # create table
+    table = []
+
+    # regex for pokemon
+    pokemonPattern = re.compile(r"V\d{4}_POKEMON_[A-Z0-9]+$")
+
+    # check all data in filePath
+    with open(filePath, 'r') as f:
+        for entry in json.load(f):
+            templateId = entry.get("templateId")
+            
+            # compare to regex pattern
+            if pokemonPattern.match(templateId):
+                
+                # obtain data
+                pokemonSettings = entry.get("data", {}).get("pokemonSettings", {})
+
+                pokemonId = int(entry.get("templateId", "")[1:5].lstrip("0"))
+                pokemonName = pokemonSettings.get("pokemonId", "")
+
+                pokemonType = pokemonSettings.get("type", "")
+                pokemonType2 = pokemonSettings.get("type2", "")
+
+                stats = pokemonSettings.get("stats", {})
+                baseStamina = int(stats.get("baseStamina", 0))
+                baseAttack = int(stats.get("baseAttack", 0))
+                baseDefense = int(stats.get("baseDefense", 0))
+
+                quickMoves = pokemonSettings.get("quickMoves", [])
+                eliteQuickMove = pokemonSettings.get("eliteQuickMove", [])
+                cinematicMoves = pokemonSettings.get("cinematicMoves", [])
+                eliteCinematicMove = pokemonSettings.get("eliteCinematicMove", [])
+
+
+                newEntry = Pokemon(id = pokemonId, name=pokemonName, type=pokemonType, type2=pokemonType2, stamina=baseStamina, attack=baseAttack, defense=baseDefense, fastMoves=quickMoves, eliteFastMove=eliteQuickMove, chargedMoves=cinematicMoves, eliteChargedMove=eliteCinematicMove)
+                
                 table.append(newEntry)
 
-def findByName(name : str):
-    split = name.find(" ")
-    if split != -1:
-        form = name[0:split]
-        name = name[split + 1:]
+    return table
 
-        if form == "Mega" or form == "Primal":
-            form = f"{form} {name}"
-    else:
-        form = " "
+def createMoveTable(filePath : str):
+    # create table
+    table = []
 
-    nameFound = False
-    for pokemon in table:
-        if pokemon.name == name and (form == " " or pokemon.form == form):
-            nameFound = True
-            returnPokemon = pokemon
-            break
+    # regex for moves
+    movePattern = re.compile(r"COMBAT_V\d{4}_MOVE_[A-Z0-9_]+")
 
-    if not nameFound:
-        raise ValueError(f"Name not found: \"{name}\"")
+    # check all data in filePath
+    with open(filePath, 'r') as f:
+        for entry in json.load(f):
+            templateId = entry.get("templateId")
+            
+            # compare to regex pattern
+            if movePattern.match(templateId):
+                
+                # obtain data
+                combatMove = entry.get("data", {}).get("combatMove", {})
 
-    return returnPokemon
+                uniqueId = str(combatMove.get("uniqueId", "")).replace("_FAST", "")
+                type = combatMove.get("type", "")
+                
+                power = float(combatMove.get("power", 0))
+                energyDelta = int(combatMove.get("energyDelta", 0))
 
-def findByNum(num : str):
-    nameFound = False
-    for pokemon in table:
-        if pokemon.id == num:
-            nameFound = True
-            returnPokemon = pokemon
-            break
+                newEntry = Move(name=uniqueId, type=type, power=power, energy=energyDelta)
+                
+                table.append(newEntry)
 
-    if not nameFound:
-        raise ValueError(f"Num not found: \"{num}\"")
+    return table
+
+def createCPTable(filePath : str):
+    # check all data in filePath
+    with open(filePath, 'r') as f:
+        for entry in json.load(f):
+            templateId = entry.get("templateId")
+            
+            # compare to regex pattern
+            if templateId == "PLAYER_LEVEL_SETTINGS":
+                
+                # obtain data
+                multipliers = entry.get("data", {}).get("playerLevel", {}).get("cpMultiplier", {})
+                return multipliers
+
+    print(f"Table could not be found")
+    return None
+
+def createTypeTable(filePath : str):
+    # create table
+    table = []
+
+    # regex for types
+    # ex: "POKEMON_TYPE_NORMAL"
+    movePattern = re.compile(r"POKEMON_TYPE_[A-Z0-9_]+")
+
+    # check all data in filePath
+    with open(filePath, 'r') as f:
+        for entry in json.load(f):
+            templateId = entry.get("templateId")
+            
+            # compare to regex pattern
+            if movePattern.match(templateId):
+                
+                # obtain data
+                # name = entry.get("templateId")
+                attackScalar = entry.get("data", {}).get("typeEffective", {}).get("attackScalar", [])
+
+                table.append(attackScalar)
+
+    return table
+
+# get data from table
+def getPokemon(query, col : str = "name"):
+    if isinstance(query, str):
+        query = query.upper()
+
+    for index, data in enumerate(pokedex):
+        if getattr(data, col, None) == query:
+            return pokedex[index]
     
-    return returnPokemon
+    print(f"'{query}' not found in col: {col}")
+    return None
 
-def calculateDamage(pokemon : Pokemon, basePower : int, atkIV : int = 10):
-    actualAttack = pokemon.attack + atkIV
-    actualDamage =  int(((0.5 * basePower * actualAttack) // 1) + 1)
+def getMove(query, col : str = "name"):
+    if isinstance(query, str):
+        query = query.upper().replace("_FAST", "")
 
-    return actualDamage
- 
+    for index, data in enumerate(moves):
+        if getattr(data, col, None) == query:
+            return moves[index]
+    
+    print(f"'{query}' not found in col: {col}")
+    return None
+
+def getTypeMatchup(attackerType, defenderType):
+    attackingIndicies = ["normal","fighting","flying","poison","ground","rock",
+                         "bug","ghost","steel","fire","water","grass",
+                         "electric","psychic","ice","dragon","dark","fairy"]
+    attackingIndicies.sort()
+    
+    defendingIndicies = ["normal","fighting","flying","poison","ground","rock",
+                         "bug","ghost","steel","fire","water","grass",
+                         "electric","psychic","ice","dragon","dark","fairy"]
+
+    attackingIndex = attackingIndicies.index(attackerType.replace("POKEMON_TYPE_", "").lower())
+    defendingIndex = defendingIndicies.index(defenderType.replace("POKEMON_TYPE_", "").lower())
+    
+    return typeChart[attackingIndex][defendingIndex]
+    
+# print table
+def printTypeTable():
+    print()
+
+    # defendingTypes = ["normal","fighting","flying","poison","ground","rock",
+    #                   "bug","ghost","steel","fire","water","grass",
+    #                   "electric","psychic","ice","dragon","dark","fairy"]
+    
+    # for defendingType in defendingTypes:
+    #     print(defendingType, end=",")
+    # print()
+
+    # for row in typeChart:
+    #     typing = row[0]
+    #     chart = row[1]
+        
+    #     print(typing, end=",")
+    #     for scalar in chart:
+    #         print(scalar, end=",")
+    #     print()
+
+# build table of pokemon
+pokedex = createPokemonTable(dataPath)
+
+# build table of moves
+moves = createMoveTable(dataPath)
+moves.sort()
+
+# build cp multipliers table
+cpMultipliers = createCPTable(dataPath)
+
+# build type table
+typeChart = createTypeTable(dataPath)
+
 if __name__ == "__main__":
     main()
